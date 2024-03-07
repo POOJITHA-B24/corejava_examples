0cfd3e1cfe4d01b79dc6f2763dfb21f2e007566e
pooja <pooja@LAPTOP-6KMQL8U3>
2024-03-07 15:13:01 +0530

    second project

---
+++ corejava_examples/src/day2_oops/Executer.java
@@ -0,0 +1,16 @@
+package day2_oops;
+
+public class Executer {
+
+	public static void main(String[] args) {
+  customer c1=new customer();
+  c1.setCid(112);
+  c1.setCname("arun");
+  c1.setAddress("theni");
+  System.out.println("customer id:"+c1.getCid()+"customer name:"+c1.getCname()+"customer address:"+c1.getAddress());
+  
+	}
+
+	}
+
+
---
+++ corejava_examples/src/day2_oops/customer.java
@@ -0,0 +1,34 @@
+package day2_oops;
+
+public class customer {
+	private int cid;
+	private String cname;
+	private String address;
+	public void setCid(int cid)
+	{
+		this.cid=cid;
+	}
+	public int getCid()
+	{
+		return cid;
+	}
+	public String getCname()
+	{
+		return cname;
+	}
+	public void setCname(String cname)
+	{
+		this.cid=cid;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	public void setAddress(String address)
+	{
+		this.address=address;
+	}
+}
+	
+
+
